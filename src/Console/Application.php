4d269dc12c7c1b9e9df4f86bf0e4d554c6cdbdf0
Gilmar Pupo <g@g1mr.com>
2016-09-28 10:06:02 -0300

    Console: esqueleto

---
+++ src/Console/Application.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of gpupo/mercadolivre-sdk
+ * Created by Gilmar Pupo <g@g1mr.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <http://www.g1mr.com/>.
+ */
+
+namespace Gpupo\MercadolivreSdk\Console;
+
+use Gpupo\CommonSdk\Console\AbstractApplication;
+use Gpupo\MercadolivreSdk\Factory;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+final class Application extends AbstractApplication
+{
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<bg=green;options=bold>gpupo/mercadolivre-sdk</>');
+        $output->writeln('<options=bold>Atenção! Esta aplicação é apenas uma '
+        .'ferramenta de apoio ao desenvolvedor e não deve ser usada no ambiente de produção!'
+        .'</>');
+
+        return parent::doRun($input, $output);
+    }
+
+    protected $commonParameters = [
+        [
+            'key' => 'client_id',
+        ],
+        [
+            'key' => 'access_token',
+        ],
+        [
+            'key'     => 'env',
+            'options' => ['sandbox', 'api'],
+            'default' => 'sandbox',
+            'name'    => 'Version',
+        ],
+        [
+            'key'     => 'sslVersion',
+            'options' => ['SecureTransport', 'TLS'],
+            'default' => 'SecureTransport',
+            'name'    => 'SSL Version',
+        ],
+        [
+            'key'     => 'registerPath',
+            'default' => false,
+        ],
+    ];
+
+    public function factorySdk(array $options, $loggerChannel = 'bin', $verbose = false)
+    {
+        return  Factory::getInstance()->setup($options, $this->factoryLogger($loggerChannel, $verbose));
+    }
+}
---
+++ src/Console/Command/AbstractCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of gpupo/mercadolivre-sdk
+ * Created by Gilmar Pupo <g@g1mr.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <http://www.g1mr.com/>.
+ */
+
+namespace Gpupo\MercadolivreSdk\Console\Command;
+
+use Gpupo\MercadolivreSdk\Console\Application;
+
+/**
+ * @codeCoverageIgnore
+ */
+abstract class AbstractCommand
+{
+    protected $list = ['main'];
+
+    protected $app;
+
+    protected function getApp()
+    {
+        return $this->app;
+    }
+
+    public function append(Application $app)
+    {
+        $this->app = $app;
+
+        foreach ($this->list as $i) {
+            $this->$i($this->getApp());
+        }
+
+        return $this->getApp();
+    }
+}
