7f11840c7ee2c5156e48e4dd004df43b8328717a
Alison Valente <alisonvm@gmail.com>
2016-12-12 17:52:38 -0200

    fix maps

---
+++ src/Entity/Order/map/status.map.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Este arquivo é parte do Sepha/Hub Ecommerce.
+ * A cópia não autorizada deste arquivo, por qualquer meio, é estritamente proibida.
+ * @copyright NovoVarejoPontoCom <ti.inovacao@sepha.com.br> - All Rights Reserved.
+ * @license Proprietary and confidential
+ * @version 2.0
+ * @require php >=5.6
+ * {@release_id}
+ * Atenção:
+ *     - A codificação deste arquivo deve permanecer UTF-8;
+ *     - Este bloco de comentário não deve ser editado;
+ *     - Utilizar short array syntax e yoda conditions;
+ *     - Sempre utilizar ``php-cs-fixer fix .`` para normalizar o código
+ */
+
+return [
+  'APPROVED'    => 'PAID',
+  'PROCESSING' => 'PROCESSING',
+  'INVOICED'    => 'INVOICED',
+  'SHIPPED'     => 'SHIPPED',
+  'DELIVERED'    => 'DELIVERED',
+  'CANCELED'   => 'CANCELED',
+];
