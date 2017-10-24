53701fb2f81791fcec0a17cf8886642d3df22889
Gilmar Pupo <contact@gpupo.com>
2017-10-24 11:20:02 -0200

    url/email update

--- Resources/doc/links.md
+++ Resources/doc/links.md
@@ -5,8 +5,8 @@
 * [Mercadolivre-sdk Composer Package](https://packagist.org/packages/gpupo/mercadolivre-sdk) no packagist.org
 * [Documentação Oficial](http://developers.mercadolibre.com/api-docs/)
 * [SDK Oficial](https://github.com/mercadolibre/php-sdk)
-* [Marketplace-bundle Composer Package](http://www.g1mr.com/MarkethubBundle/) - Integração deste pacote com Symfony
-* [Outras SDKs para o Ecommerce do Brasil](http://www.g1mr.com/common-sdk/)
+* [Marketplace-bundle Composer Package](https://opensource.gpupo.com/MarkethubBundle/) - Integração deste pacote com Symfony
+* [Outras SDKs para o Ecommerce do Brasil](https://opensource.gpupo.com/common-sdk/)
 
 
 
--- src/Factory.php
+++ src/Factory.php
@@ -2,14 +2,14 @@
 
 /*
  * This file is part of gpupo/mercadolivre-sdk
- * Created by Gilmar Pupo <g@g1mr.com>
+ * Created by Gilmar Pupo <contact@gpupo.com>
  * For the information of copyright and license you should read the file
  * LICENSE which is distributed with this source code.
  * Para a informação dos direitos autorais e de licença você deve ler o arquivo
  * LICENSE que é distribuído com este código-fonte.
  * Para obtener la información de los derechos de autor y la licencia debe leer
  * el archivo LICENSE que se distribuye con el código fuente.
- * For more information, see <http://www.g1mr.com/>.
+ * For more information, see <https://opensource.gpupo.com/>.
  */
 
 namespace Gpupo\MercadolivreSdk;
