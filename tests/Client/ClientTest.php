c99caec81037d3b747c56d5704008493cc170e3e
Gilmar Pupo <contact@gpupo.com>
2018-10-10 14:52:01 -0300

    Fix test

--- tests/Client/ClientTest.php
+++ tests/Client/ClientTest.php
@@ -43,7 +43,7 @@ class ClientTest extends TestCaseAbstract
     public function testGerenciaUriDeRecurso(ClientInterface $client)
     {
         $this->assertSame(
-            'https://api.mercadolibre.com/items/MLB12343412/descriptions',
+            '/items/MLB12343412/descriptions',
             $client->getResourceUri('/items/MLB12343412/descriptions')
         );
     }
