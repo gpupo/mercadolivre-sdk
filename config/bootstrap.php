bd52c245d1ca89299a48cba07ff25edeb3131fba
Gilmar Pupo <contact@gpupo.com>
2019-12-10 10:53:01 -0300

    Update Dotenv config to the new version

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -26,5 +26,5 @@ if (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } else {
     // load all the .env files
-    (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
+    (new Dotenv(true))->loadEnv(dirname(__DIR__).'/.env');
 }
